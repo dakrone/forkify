5a6979d898b72d2db6d6157fb948e455ff63bcd0
dakrone <lee@writequit.org>
2009-07-10 00:50:52 -0600

    Initial work on DRb/Rinda::TupleSpace process task queue for a pool of processes instead of serial-forking.

--- lib/forkify.rb
+++ lib/forkify.rb
@@ -1,7 +1,103 @@
-FORKIFY_DEBUG = false
+FORKIFY_DEBUG = true
 require 'pp' if FORKIFY_DEBUG
 
+require 'rinda/tuplespace'
+require 'pp'
+
 module Enumerable
+
+  def forkify2 procs = 5, &block
+    puts "Forkify Class: #{self.class}" if FORKIFY_DEBUG
+    if self === Array
+      items = self
+    else
+      begin
+        items = self.to_a
+      rescue NoMethodError => e
+        raise NoMethodError, "Unable to coerce #{self.inspect} to an Array type."
+      end
+    end
+
+    result_tuples = []
+    results = []
+    pids = []
+    items_remaining = items.size
+
+    num_procs = procs
+    num_procs = items_remaining if items_remaining < procs
+
+    num_procs.times do
+
+      pid = fork
+      unless pid
+        #puts "service?"
+        DRb.start_service
+        ts = Rinda::TupleSpaceProxy.new(DRbObject.new_with_uri('druby://localhost:53421'))
+
+        loop do
+          puts "Taking..."
+          item = ts.take([:enum, nil, nil])
+          pp "Got => #{item}"
+
+          # our termination tuple
+          break if item == [:enum, -1, nil]
+          result =
+            begin
+              block.call(item[2])
+            rescue Object => e
+              e
+            end
+
+          # return result
+          puts "writing result: #{result.inspect}"
+          ts.write([:result, item[1], result])
+
+          puts "loop bottom"
+        end
+        DRb.stop_service
+
+        puts "child #{$$} dying"
+        exit!
+      end
+
+      pids << pid
+    end
+
+    pts = Rinda::TupleSpace.new
+
+    # write termination tuples
+    items.size.times do
+      puts "pushing terminator"
+      pts.write([:enum, -1, nil])
+    end
+
+    items.each_with_index { |item, index|
+      puts "pushing data"
+      pts.write([:enum, index, item])
+    }
+
+    DRb.start_service('druby://localhost:53421', pts)
+
+    sleep(1)
+    # TODO: get results, where?
+    items.size.times do
+      puts "grabbing a result..."
+      result_tuples << pts.take([:result, nil, nil])
+    end
+
+    pp "Waiting for pids: #{pids.inspect}" if FORKIFY_DEBUG
+    pids.each { |p| Process.waitpid(p)  }
+
+    DRb.stop_service
+
+    # gather results and sort them
+    result_tuples.map { |t|
+      puts "results[#{t[1]}] = #{t[2]}"
+      results[t[1]] = t[2]
+    }
+
+    return results
+  end
 
   #
   # Forkify will process _block_'s actions using processes. If no number of processes is
@@ -77,7 +173,7 @@ module Enumerable
       #datawaiting_pipes = Kernel.select(rpipes, wpipes, nil, 2)
       #readwaiting_pipes = datawaiting_pipes[0]
       #writewaiting_pipes = datawaiting_pipes[1]
-      
+
       # Switch to 2 selects instead of 1
       #readwaiting_pipes = Kernel.select(rpipes, nil, nil, 2)[0]
       #writewaiting_pipes = Kernel.select(nil, wpipes, nil, 2)[1]
