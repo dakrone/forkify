79fb4c78f5f93a569b3b7e80ced6071e0ce8ddca
dakrone <lee@writequit.org>
2009-08-13 16:56:34 -0600

    added spec opts and renamed tests to be much clearer (hopefully)

--- spec/forkify_spec.rb
+++ spec/forkify_spec.rb
@@ -4,7 +4,7 @@ require 'spec'
 require 'forkify'
 
 describe 'forkify' do
-  it 'should fork serially and take less time than normal' do
+  it 'should fork serially and take less time' do
     time1 = Time.now
     r = [1, 2, 3].forkify(3) { |n| sleep(1) }
     time2 = Time.now
@@ -12,7 +12,7 @@ describe 'forkify' do
     (time2 - time1).should < 3
   end
 
-  it 'pool timings' do
+  it 'should fork with a pool and take less time' do
     time1 = Time.now
     r = [1, 2, 3].forkify(:procs => 3, :method => :pool) { |n| sleep(1) }
     time2 = Time.now
@@ -20,15 +20,15 @@ describe 'forkify' do
     (time2 - time1).should < 3
   end
 
-  it 'serial array results' do
+  it 'should return an array of results from a serial fork' do
     [1, 2, 3].forkify { |n| n * 2 }.should == [2, 4, 6]
   end
 
-  it 'pool array results' do
+  it 'should return an array of results from a pool fork' do
     [1, 2, 3].forkify(:method => :pool) { |n| n * 2 }.should == [2, 4, 6]
   end
 
-  it 'serial hash results' do
+  it 'should return a hash of results from a serial fork' do
     r = {:a => 1, :b => 2, :c => 3}.forkify { |k, v| [k, v*2] }
     r.should include([:a, 2])
     r.should include([:b, 4])
@@ -36,7 +36,7 @@ describe 'forkify' do
     r.size.should == 3
   end
 
-  it 'pool hash results' do
+  it 'should return a hash of results from a pool fork' do
     r = {:a => 1, :b => 2, :c => 3}.forkify(:method => :pool) { |k, v| [k, v*2] }
     r.should include([:a, 2])
     r.should include([:b, 4])
@@ -44,11 +44,11 @@ describe 'forkify' do
     r.size.should == 3
   end
 
-  it 'serial array of nils' do
+  it 'should return an array of nils from a serial nil fork' do
     [nil, nil].forkify { |n| n }.should == [nil, nil]
   end
 
-  it 'pool array of nils' do
+  it 'should return an array of nils from a pool nil fork' do
     [nil, nil].forkify(:method => :pool) { |n| n }.should == [nil, nil]
   end
 end
